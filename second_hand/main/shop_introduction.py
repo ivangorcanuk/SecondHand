2be4ed6155b77863920cd467014d0231c4cca5a0
ivangorcanuk <ivangorcanuk464@gmail.com>
2023-06-16 23:10:07 +0300

    Adapted views to the base

--- second_hand/main/shop_introduction.py
+++ second_hand/main/shop_introduction.py
@@ -12,7 +12,7 @@ class Store:
                      6: 'Вс'}
 
     def __init__(self, id_store, name_store, country, city, area, street, house, floor,
-                 number_phone, number_stars, rating, store_network, open_hours):
+                 number_phone, number_stars, rating, store_network, open_hours, list_promotion_days):
         self.id = id_store
         self.name_store = name_store
         self.country = country
@@ -28,7 +28,9 @@ class Store:
         self.list_open_hours = [open_hours.monday, open_hours.tuesday, open_hours.wednesday,
                                 open_hours.thursday, open_hours.friday, open_hours.saturday,
                                 open_hours.sunday]
+        self.list_promotion_days = list_promotion_days
         self.opening_hours_today_text = self.get_day_of_week()  # готовая строка для отображения рабочего времени
+        self.discount_today = list_promotion_days[datetime.weekday(datetime.now())]
         self.list_days_open_hours = self.prepare_week_schedule()  # заполнили список днями с рабочим расписанием
 
     def get_day_of_week(self, p_week_day=-1):
@@ -38,31 +40,35 @@ class Store:
         work_time_value = self.list_open_hours[week_day]
         return work_time_value[:2] + ':00 - ' + work_time_value[2:] + ':00'
 
-    def prepare_week_schedule(self):
+    def get_time_str(self, time, first_day=str(), second_day=str()):
+        time = time[:2] + ':00 - ' + time[2:] + ':00'
+        if first_day != str() and second_day == str():
+            forma = first_day + ': ' + time
+        elif first_day != str() and second_day != str():
+            forma = first_day + '-' + second_day + ': ' + time
+        else:
+            return None
+        return forma
+
+    def prepare_week_schedule(self):  # формирует список с готовыми сторакми для отображения пользователям
         is_new_sequence = True  # это новая последовательность? (да/нет)
         first_day = int()
         list_days_open_hours = list()
-        for i in range(6):
-            if self.list_open_hours[i] == self.list_open_hours[i+1]:
-                if is_new_sequence:
-                    first_day = i
+        for i in range(1, 7):
+            if is_new_sequence:
+                first_day = i - 1
+            if self.list_open_hours[i - 1] == self.list_open_hours[i]:
                 is_new_sequence = False
-            else:
-                time = self.list_open_hours[i]
-                time = time[:2] + ':00 - ' + time[2:] + ':00'
-                forma = self.dict_weekdays[first_day] + '-' + self.dict_weekdays[i] + ': ' + time
+            elif self.list_open_hours[i - 1] != self.list_open_hours[i] or i == 6:
+                if first_day != i - 1:
+                    forma = self.get_time_str(self.list_open_hours[i - 1], self.dict_weekdays[first_day], self.dict_weekdays[i - 1])
+                else:
+                    forma = self.get_time_str(self.list_open_hours[i - 1], self.dict_weekdays[first_day])
+                list_days_open_hours.append(forma)
                 is_new_sequence = True
-                list_days_open_hours.append(forma)
-            if len(self.list_open_hours) == i + 2:
-                time = self.list_open_hours[i]
-                time = time[:2] + ':00 - ' + time[2:] + ':00'
-                forma = self.dict_weekdays[first_day] + '-' + self.dict_weekdays[i] + ': ' + time
-                list_days_open_hours.append(forma)
-                break
+        if self.list_open_hours[-1] != self.list_open_hours[-2]:
+            list_days_open_hours.append(self.get_time_str(self.list_open_hours[-1], self.dict_weekdays[6]))
+        else:
+            list_days_open_hours.append(self.get_time_str(self.list_open_hours[-1], self.dict_weekdays[first_day], self.dict_weekdays[6]))
         return list_days_open_hours
-
-# пн-вт  10:00-20:00
-# ср-чт  10:00-20:00
-# пт-сб  10:00-20:00
-# вс     10:00-20:00
 

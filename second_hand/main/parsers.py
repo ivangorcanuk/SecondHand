f3b88ca9e7ed763de7684d094aaaefc0a2f7256e
ivangorcanuk <ivangorcanuk464@gmail.com>
2023-08-27 18:27:20 +0300

    Implemented data parsing of the Adzenne trading network

--- second_hand/main/parsers.py
+++ second_hand/main/parsers.py
@@ -89,51 +89,46 @@ class EconomCityParser:
         return dict_shop_data
 
 
-# class Adzenne:
-#     url = 'https://second-hand.by/kramy/'
-#     headers = {
-#         'Accept': 'application/font-woff2;q=1.0,application/font-woff;q=0.9,*/*;q=0.8',
-#         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/116.0'
-#     }
-#
-#     def __init__(self):
-#         self.__dikt_url_minsk_shops = None
-#
-#     def __get_url_all_shops_network(self):  # вернули ссылку со всеми магазинами города Минска
-#         req = requests.get(self.url, headers=self.headers)
-#         soup = BS(req.content, 'lxml')
-#         #all_shop = soup.find(class_='shops__list not-list')
-#         all_shop1 = soup.find_all('a', class_='shops__link')
-#         dict_shop = dict()
-#         for i in all_shop1:
-#             if i.text == '\n':
-#                 continue
-#             dict_shop[i.text.replace('\n', '')] = 'https://secondhand.by/' + i.get('href')
-#         return dict_shop
-#
-#     def get_data(self):
-#         self.__dikt_url_minsk_shops = self.__get_url_all_shops_network()
-#         count = 0
-#         dict_shop_data = dict()
-#         for key, value in self.__dikt_url_minsk_shops.items():
-#             if key == 'Рокоссовского, 49':  # Рокоссовского, 49
-#                 req_1 = requests.get(value, headers=self.headers)
-#                 soup_1 = BS(req_1.content, 'lxml')
-#                 work_time = soup_1.find(class_="shopMap__info-description")
-#                 all_calendar = soup_1.find("tbody")
-#                 list_days = all_calendar.find_all("td")
-#                 # f = all_work_time.text.replace('\n', '')  # получили склеиную строку
-#                 list_days.append(work_time)
-#                 dict_shop_data[key] = list_days
-#                 count += 1
-#         return dict_shop_data
-#
-# req = requests.get(Adzenne.url, headers=Adzenne.headers)
-# soup = BS(req.content, 'lxml')
-# #print(soup)
-# all_shop1 = soup.find_all(class_='wpb_column vc_column_container vc_col-sm-2 vc_hidden-md vc_hidden-sm vc_hidden-xs')
-# for i in all_shop1:
-#     #a = 'https://second-hand.by/kramy/' + i.get('href')
-#     print(i)
+class Adzenne:
+    url = 'https://second-hand.by/kramy/'
+    headers = {
+        'Accept': 'application/font-woff2;q=1.0,application/font-woff;q=0.9,*/*;q=0.8',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/116.0'
+    }
+
+    def __init__(self):
+        self.__dikt_url_minsk_shops = None
+
+    def __get_url_all_shops_network(self):  # вернули ссылку со всеми магазинами города Минска
+        req = requests.get(self.url, headers=self.headers)
+        soup = BS(req.content, 'lxml')
+        all_shop = soup.find(class_='wpb_column vc_column_container vc_col-sm-12 vc_hidden-lg')
+        all_shop1 = all_shop.find_all('a')
+        dict_shop = dict()
+        for i in all_shop1:
+            if i.text == '\n':
+                continue
+            dict_shop[i.text] = 'https://second-hand.by/kramy/' + i.get('href')
+            #print(i.text, 'https://second-hand.by/kramy/' + i.get('href'))
+        return dict_shop
+
+    def get_data(self):
+        self.__dikt_url_minsk_shops = self.__get_url_all_shops_network()
+        count = 0
+        dict_shop_data = dict()
+        for key, value in self.__dikt_url_minsk_shops.items():
+            if key != 'вул. С. Ясеніна, 27':
+                req_1 = requests.get(value, headers=self.headers)
+                soup_1 = BS(req_1.content, 'lxml')
+                working_month = soup_1.find('tbody', class_="simcal-month simcal-month-8")
+                list_all_calendar = working_month.find_all("td")
+                all_calendar = soup_1.find('div', class_="wpb_wrapper")
+                work_time = re.search(r'Крама працуе:\n*(.+\n*.*)\n*Прыпынак', all_calendar.text)
+                list_all_calendar.append(work_time.group(1))
+                dict_shop_data[key] = list_all_calendar
+                count += 1
+        return dict_shop_data
+
+
 class Megahand:
     pass

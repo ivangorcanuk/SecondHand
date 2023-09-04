d3b58e63091f995ca68f5652cce6f3feff875a0a
ivangorcanuk <ivangorcanuk464@gmail.com>
2023-09-04 18:47:36 +0300

    Edited the code including renamed the variables more correctly.

--- second_hand/main/parsers.py
+++ second_hand/main/parsers.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup as BS
 import re
 import json
-from datetime import datetime, date, timedelta
 
 
 class ModaMaxParser:
@@ -13,7 +12,7 @@ class ModaMaxParser:
     }
 
     def __init__(self):
-        self.__url_minsk_shop = None
+        self.__url_shops_in_minsk = None
         self.__dikt_url_minsk_shops = None
 
     def __get_url_all_shops_network(self):  # вернули ссылку со всеми магазинами города Минска
@@ -24,29 +23,26 @@ class ModaMaxParser:
         return link_minsk_shop
 
     def __get_url_all_shops(self, url_minsk_shop):  # вернули словарь магазинов города Минска
-        req_1 = requests.get(url_minsk_shop, headers=self.headers)
-        soup_1 = BS(req_1.content, 'lxml')
-        link_shops = soup_1.find_all(class_="shopCard")
-        dict_shop = dict()  # словарь для хранения магазинов, ключ - адрес, значение - ссылка
+        req = requests.get(url_minsk_shop, headers=self.headers)
+        soup = BS(req.content, 'lxml')
+        link_shops = soup.find_all(class_="shopCard")
+        dict_shops = dict()  # словарь для хранения магазинов, ключ - адрес, значение - ссылка
         for i in link_shops:
             street_house = re.search(r'Минск, (.+)  \n', i.text)
-            #print(f'Тута \"{street_house.group(1)}\"')
-            dict_shop[street_house.group(1)] = 'https://modamax.by/' + i.get('href')
-        return dict_shop
+            dict_shops[street_house.group(1)] = 'https://modamax.by/' + i.get('href')
+        return dict_shops
 
     def get_data(self):
-        self.__url_minsk_shop = self.__get_url_all_shops_network()
-        self.__dikt_url_minsk_shops = self.__get_url_all_shops(self.__url_minsk_shop)
-        count = 0
+        self.__url_shops_in_minsk = self.__get_url_all_shops_network()
+        self.__dikt_url_minsk_shops = self.__get_url_all_shops(self.__url_shops_in_minsk)
         dict_shop_data = dict()
         for key, value in self.__dikt_url_minsk_shops.items():
-            req_2 = requests.get(value, headers=self.headers)
-            soup_2 = BS(req_2.content, 'lxml')
-            open_hours = soup_2.find(class_='shopInfo__row-content')
-            list_prices = soup_2.find_all(class_='shopPrice__cell')
+            req = requests.get(value, headers=self.headers)
+            soup = BS(req.content, 'lxml')
+            open_hours = soup.find(class_='shopInfo__row-content')
+            list_prices = soup.find_all(class_='shopPrice__cell')
             list_prices.append(open_hours)
             dict_shop_data[key] = list_prices
-            count += 1
         return dict_shop_data
 
 
@@ -63,31 +59,26 @@ class EconomCityParser:
     def __get_url_all_shops_network(self):  # вернули ссылку со всеми магазинами города Минска
         req = requests.get(self.url, headers=self.headers)
         soup = BS(req.content, 'lxml')
-        all_shop = soup.find(class_='shops__list not-list')
-        all_shop1 = all_shop.find_all(class_='shops__link')
-        dict_shop = dict()
-        for i in all_shop1:
-            if i.text == '\n':
-                continue
-            dict_shop[i.text.replace('\n', '')] = 'https://secondhand.by/' + i.get('href')
-        return dict_shop
+        shops_block = soup.find(class_='shops__list not-list')
+        all_shops = shops_block.find_all(class_='shops__link')
+        dict_shops = dict()
+        for i in all_shops:
+            if i.text != '\n':
+                dict_shops[i.text.replace('\n', '')] = 'https://secondhand.by/' + i.get('href')
+        return dict_shops
 
     def get_data(self):
         self.__dikt_url_minsk_shops = self.__get_url_all_shops_network()
-        count = 0
-        dict_shop_data = dict()
+        dict_shops_data = dict()
         for key, value in self.__dikt_url_minsk_shops.items():
-            #if key == 'Сухаревская, 5':  # Сухаревская, 5
-            req_1 = requests.get(value, headers=self.headers)
-            soup_1 = BS(req_1.content, 'lxml')
-            work_time = soup_1.find(class_="shopMap__info-description")
-            all_calendar = soup_1.find("tbody")
-            list_days = all_calendar.find_all("td")
-            # f = all_work_time.text.replace('\n', '')  # получили склеиную строку
-            list_days.append(work_time)
-            dict_shop_data[key] = list_days
-            count += 1
-        return dict_shop_data
+            req = requests.get(value, headers=self.headers)
+            soup = BS(req.content, 'lxml')
+            open_hours = soup.find(class_="shopMap__info-description")
+            calendar_block = soup.find("tbody")
+            list_days = calendar_block.find_all("td")
+            list_days.append(open_hours)
+            dict_shops_data[key] = list_days
+        return dict_shops_data
 
 
 class AdzenneParser:
@@ -107,32 +98,28 @@ class AdzenneParser:
         all_shop_link = list()
         for i in all_shop:
             if 'Крама' in i.text:
-                a = i.find_all('a')
-                all_shop_link.append(a[1])
+                tags = i.find_all('a')
+                all_shop_link.append(tags[1])
             if 'Салігорск' in i.text:
                 break
         dict_shop = dict()
         for i in all_shop_link:
             dict_shop[i.text] = i.get('href')
-            #print(i.text, i.get('href'))
         return dict_shop
 
     def get_data(self):
         self.__dikt_url_minsk_shops = self.__get_url_all_shops_network()
-        count = 0
         dict_shop_data = dict()
         for key, value in self.__dikt_url_minsk_shops.items():
-            #if key == 'Крама па вул. В. Харужай, 8':
-            #if count < 1:
-            req_1 = requests.get(value, headers=self.headers)
-            soup_1 = BS(req_1.content, 'lxml')
-            working_month = soup_1.find('tbody', class_="simcal-month simcal-month-8")
-            list_all_calendar = working_month.find_all("td")
-            all_calendar = soup_1.find('div', class_="wpb_wrapper")
+            print(key, value)
+            req1 = requests.get(value, headers=self.headers)
+            soup1 = BS(req1.content, 'lxml')
+            calendar_block = soup1.find('table', class_="simcal-calendar-grid")
+            list_all_days = calendar_block.find_all("td")
+            all_calendar = soup1.find('div', class_="wpb_wrapper")
             work_time = re.search(r'Крама працуе:\n*(.+\n*.*)\n*Прыпынак', all_calendar.text)
-            list_all_calendar.append(work_time.group(1))
-            dict_shop_data[key] = list_all_calendar
-            count += 1
+            list_all_days.append(work_time.group(1))
+            dict_shop_data[key] = list_all_days
         return dict_shop_data
 
 
@@ -149,42 +136,37 @@ class MegahandParser:
     def __get_url_all_shops_network(self):  # вернули ссылку со всеми магазинами города Минска
         req = requests.get(self.url, headers=self.headers)
         soup = BS(req.content, 'lxml')
-        all_shop = soup.find_all(class_='sity_magazin')
-        dict_shop = dict()
-        for i in all_shop:
-            if 'магазин МЕГАХЕНД в Минске' in i.text:
-                link = i.find('a', class_='public-life__image-link')
-                address = i.find(class_='top_shop_blok_desc')
-                dict_shop[address.text] = link.get('href')
-        return dict_shop
+        all_shops = soup.find_all(class_='sity_magazin')
+        dict_shops = dict()
+        for url in all_shops:
+            if 'магазин МЕГАХЕНД в Минске' in url.text:
+                link = url.find('a', class_='public-life__image-link')
+                address = url.find(class_='top_shop_blok_desc')
+                dict_shops[address.text] = link.get('href')
+        return dict_shops
 
     def get_data(self):
         self.__dikt_url_minsk_shops = self.__get_url_all_shops_network()
-        count = 0
-        dict_shop_data = dict()
+        dict_shops_data = dict()
         for key, value in self.__dikt_url_minsk_shops.items():
-            if count < 1:
-                req_1 = requests.get(value, headers=self.headers)
-                soup_1 = BS(req_1.content, 'lxml')
-                print(soup_1)
+            req = requests.get(value, headers=self.headers)
+            soup = BS(req.content, 'lxml')
 
-                scripts_all = soup_1.find_all('script', type='text/javascript')
-                list_of_dictionaries = None
-                for script in scripts_all:
-                    script_str = str(script)
-                    if 'var data' in script_str:
-                        script_calendar = script_str[script_str.index('['):script_str.index(']')+1]
-                        list_of_dictionaries = json.loads(script_calendar)
-                        print(type(list_of_dictionaries))
+            scripts_all = soup.find_all('script', type='text/javascript')
+            list_of_dictionaries = None
+            for script in scripts_all:
+                script_str = str(script)
+                if 'var data' in script_str:
+                    script_calendar = script_str[script_str.index('['):script_str.index(']')+1]
+                    list_of_dictionaries = json.loads(script_calendar)
 
-                so = soup_1.find_all(class_="top_shop_blok")
-                work_time = None
-                for i in so:
-                    if 'Ежедневно' in i.text:
-                        work_time = i.find(class_="top_shop_blok_desc")
+            info_blocks = soup.find_all(class_="top_shop_blok")
+            open_hours = None
+            for block in info_blocks:
+                if 'Ежедневно' in block.text:
+                    open_hours = block.find(class_="top_shop_blok_desc")
 
-                list_of_dictionaries.append(work_time)
-                dict_shop_data[key] = list_of_dictionaries
-                count += 1
+            list_of_dictionaries.append(open_hours)
+            dict_shops_data[key] = list_of_dictionaries
 
-        return dict_shop_data
+        return dict_shops_data
